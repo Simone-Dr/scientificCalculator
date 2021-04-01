1823e56ee84f86afabce15c92ca773e5172a153a
Simone Dr <56234858+Simone-Dr@users.noreply.github.com>
2021-04-01 18:34:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
-//js
+var pressedEnter = false; 
+var save = ""; 
+var numbers = []; 
+
+function number(inp){
+	if (pressedEnter == false){ 
+		input.innerHTML += inp; 
+		save += inp;  
+	}
+}
+
+function ArithSym(inp){
+	if (pressedEnter == false){ 
+		input.innerHTML += inp; 
+		if (save != ""){
+		numbers.push(save);
+		}
+		save = ""; 
+		numbers.push(inp)
+	}
+
+
+} 
+
+function clr() {
+	input.innerHTML = " "; 
+
+}
+
+function solve() {
+
+	if (save != ""){
+		numbers.push(save);
+	}
+	if (pressedEnter == false){ 
+		input.innerHTML += " = "; 
+	}
+
+	pressedEnter = true; 
+	InfixToPostfix(); 
+}
+
+function InfixToPostfix() {
+	for (let i = 0; i < numbers.length; i++) {
+		console.log(numbers[i]);
+		if (isNaN(numbers[i]) == false) {
+			
+		}
+	}
+
+}
